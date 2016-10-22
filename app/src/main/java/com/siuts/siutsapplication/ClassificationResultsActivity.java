e2a8d25321facf691580a7c1af318f135518c6a4
Timo Petmanson <tpetmanson@gmail.com>
2016-10-22 14:33:20 +0300

    Added an activity that will be displaying classification results with short bird profiles

---
+++ app/src/main/java/com/siuts/siutsapplication/ClassificationResultsActivity.java
@@ -0,0 +1,15 @@
+package com.siuts.siutsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassificationResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classification_results);
+
+        // set some example data
+    }
+}
