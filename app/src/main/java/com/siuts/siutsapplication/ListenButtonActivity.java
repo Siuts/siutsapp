dde560afaf438c93f8454aa748732613b9f9e0ac
Vitalii Lakusta <lakusta96@gmail.com>
2016-10-22 20:56:34 +0300

    Add button

--- app/src/main/java/com/siuts/siutsapplication/ListenButtonActivity.java
+++ app/src/main/java/com/siuts/siutsapplication/ListenButtonActivity.java
@@ -1,17 +1,16 @@
 package com.siuts.siutsapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.saeid.fabloading.LoadingView;
 
 
 public class ListenButtonActivity extends AppCompatActivity {
 
-    @BindView(R.id.listenButton) Button listenButton;
+    @BindView(R.id.listenButton) LoadingView listenButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +18,7 @@ public class ListenButtonActivity extends AppCompatActivity {
         setContentView(R.layout.activity_listen_button);
         ButterKnife.bind(this);
 
-        listenButton.setOnClickListener((View v) -> {
-            System.out.println("hello");
-        });
+        listenButton.addAnimation(0x64B5F6, R.drawable.emberiza_citrinella, LoadingView.FROM_LEFT);
+
     }
 }
