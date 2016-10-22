b2f0513e7b93ee81d5008d11b5e50575fd58938a
Vitalii Lakusta <lakusta96@gmail.com>
2016-10-22 22:28:53 +0300

    Start animation on the start

--- app/src/main/java/com/siuts/siutsapplication/ListenButtonActivity.java
+++ app/src/main/java/com/siuts/siutsapplication/ListenButtonActivity.java
@@ -2,6 +2,7 @@ package com.siuts.siutsapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -18,10 +19,14 @@ public class ListenButtonActivity extends AppCompatActivity {
         setContentView(R.layout.activity_listen_button);
         ButterKnife.bind(this);
 
-        listenButton.addAnimation(0x64B5F6, R.drawable.emberiza_citrinella, LoadingView.FROM_LEFT);
+        listenButton.addAnimation(0x64B5F6, R.drawable.fringilla_coelebs, LoadingView.FROM_LEFT);
         listenButton.addAnimation(0x64B5ff, R.drawable.apus_apus, LoadingView.FROM_BOTTOM);
         listenButton.addAnimation(0x64B500, R.drawable.chloris_chloris, LoadingView.FROM_TOP);
         listenButton.addAnimation(0x64B5F6, R.drawable.fab_shadow, LoadingView.FROM_RIGHT);
+
+        listenButton.setOnClickListener((View v) -> {
+            listenButton.startAnimation();
+        });
 
     }
 }
