3292db99bdcea5dc36c1ef8a893c41b9a0b2b114
Timo Petmanson <tpetmanson@gmail.com>
2016-10-22 17:04:19 +0300

    Created class that should be given to the ClassificationResultAdapter

---
+++ app/src/main/java/com/siuts/siutsapplication/common/ClassifiedBird.java
@@ -0,0 +1,16 @@
+package com.siuts.siutsapplication.common;
+
+
+import lombok.Data;
+
+public @Data class ClassifiedBird {
+    private String slug;
+    private String name;
+    private String name_en;
+    private String name_et;
+    private String name_la;
+    private String description;
+    private String description_en;
+    private String description_et;
+    private String description_la;
+}
