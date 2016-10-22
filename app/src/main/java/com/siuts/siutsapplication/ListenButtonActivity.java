c684629a26fd985c2875066ba2cf3eeebb239078
Vitalii Lakusta <lakusta96@gmail.com>
2016-10-22 15:22:30 +0300

    Add "listen button" screen the main screen

---
+++ app/src/main/java/com/siuts/siutsapplication/ListenButtonActivity.java
@@ -0,0 +1,13 @@
+package com.siuts.siutsapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListenButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listen_button);
+    }
+}
