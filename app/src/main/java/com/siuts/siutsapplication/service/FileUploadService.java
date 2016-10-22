81f332e066f0bf3f9ee1fad16d4f878bbe6aafb0
Timo Petmanson <tpetmanson@gmail.com>
2016-10-23 00:02:02 +0300

    Interface for file upload service

---
+++ app/src/main/java/com/siuts/siutsapplication/service/FileUploadService.java
@@ -0,0 +1,19 @@
+package com.siuts.siutsapplication.service;
+
+import com.siuts.siutsapplication.domain.Endpoints;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.Part;
+
+
+public interface FileUploadService {
+    @Multipart
+    @POST(Endpoints.UPLOAD)
+    Call<ResponseBody> upload(@Part("description") RequestBody description,
+                              @Part MultipartBody.Part file);
+}
